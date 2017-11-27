7bcd5eefa8e7d936c9971e9596a4144ec6353f40
Benny Wang <bennywang75@hotmail.com>
2017-11-26 23:27:14 -0800

    Created basic layout and added FilePickerDialog

--- PttP/PttP/PttP.cpp
+++ PttP/PttP/PttP.cpp
@@ -2,6 +2,40 @@
 
 PttP::PttP(QWidget *parent)
 	: QMainWindow(parent)
+	, mSelectedFileName()
 {
 	ui.setupUi(this);
+	connect(ui.pushButtonSelect, &QPushButton::pressed, this, &PttP::selectFile);
+}
+
+/*-------------------------------------------------------------------------------------------------
+-- FUNCTION: selectFile()
+--
+-- DATE: November 26, 2017
+--
+-- REVISIONS: N/A
+--
+-- DESIGNER: 
+--
+-- PROGRAMMER: Benny Wang 
+--
+-- INTERFACE: void selectFile (void)
+--
+-- RETURNS: void.
+--
+-- NOTES:
+-- A Qt slot.
+--
+-- Opens a QFileDialog to retrieve the filename of the file that the user wants to transfer and
+-- saves it to an instance variable of this class.
+-------------------------------------------------------------------------------------------------*/
+void PttP::selectFile()
+{
+	mSelectedFileName = QFileDialog::getOpenFileName(
+		this,							// Parent object
+		tr("Choose File to Open"),		// Title
+		"./",							// Default directory
+		tr("Text File (*.txt)")			// File types
+	);
+	ui.labelSelectedFile->setText(mSelectedFileName);
 }
